27e42834ecaced4ac2a0b9a52c8f77715942eb98
Vebjorn Ljosa <vebjorn@ljosa.com>
2014-04-02 13:48:30 -0400

    Automated build with Jenkins

--- jenkins/build_centos6.sh
+++ jenkins/build_centos6.sh
@@ -10,7 +10,6 @@ set -e
 set -x
 
 here=$(dirname "$0")
-yum install -y python-setuptools numpy gcc python-devel java-1.7.0-openjdk-devel which
 python "$here"/get-pip.py
 pip install cython
 cd "$here"/..
