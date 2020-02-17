bd7f89cdde040b473c9ffc40f26003e0a94d0065
Lee Kamentsky <lkaments@mit.edu>
2020-02-17 09:23:58 -0500

    print is a function in Python 3
    Thanks @cclaus

--- demo/demo_nogui_with_stmt.py
+++ demo/demo_nogui_with_stmt.py
@@ -15,5 +15,5 @@ import os
 import javabridge
 
 with javabridge.vm(run_headless=True):
-    print javabridge.run_script('java.lang.String.format("Hello, %s!", greetee);', 
-                                dict(greetee='world'))
+    print(javabridge.run_script('java.lang.String.format("Hello, %s!", greetee);', 
+                                dict(greetee='world')))
